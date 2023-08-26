144b5c95c492c9918f74726b1437b18df9620a63
Todo Lodo <me@todolodo.xyz>
2023-08-26 22:44:36 +0530

    update

--- index.js
+++ index.js
@@ -24,7 +24,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// whoami
+app.get('/api/whoami', function (req, res) {
+  const headers = req.headers;
+  console.log(req.headers);
+  console.log(req.socket.remoteAddress)
+  res.json({ ipaddress: 'hello API',  language: headers['accept-language'], software: headers['user-agent']});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+  console.log('http://localhost:'+listener.address().port);
 });
